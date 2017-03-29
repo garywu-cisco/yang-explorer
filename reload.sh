26c41c7171880fd7b510f625c5903f86a8550eb6
Gary Wu <garywu@cisco.com>
2017-03-29 15:51:13 -0700

    Add the ability to reload new CXML separately

--- reload.sh
+++ reload.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [[ $NOVENV != 1 ]]; then
+    echo "Creating / Activating virtualenv .."
+    if [ -f "v/bin/activate" ]; then
+        source v/bin/activate
+    else
+        virtualenv v
+        source v/bin/activate
+    fi
+fi
+
 if [ ! -f "server/data/db.sqlite3" ]; then
     echo "Database does not exist!  Run setup.sh first!"
     exit false
--- server/explorer/management/commands/reload_cxml.py
+++ server/explorer/management/commands/reload_cxml.py
@@ -17,6 +17,7 @@ limitations under the License.
 """
 
 import os
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from explorer.models import User, UserProfile
 
@@ -24,25 +25,22 @@ class Command(BaseCommand):
     help = 'Reload UserProfile with updated CXML files'
 
     def add_arguments(self, parser):
-        print 'in add_arguments'
         parser.add_argument('--user', default='guest',
                 help='Reload UserProfiles belonging to this user')
 
     def handle(self, *args, **options):
-        print str(args)
-        print str(options)
         self._reload_cxml(username=options['user'])
 
     def _reload_cxml(self, username):
         user = User.objects.get(username=username)
 
         # delete all UserProfiles associated with this user
-        user.userprofile_set.delete()
+        user.userprofile_set.all().delete()
 
         # create new UserProfiles from CXML files
-        for f in os.listdir(os.path.join(django.conf.settings.BASE_DIR,
-                                         'data', 'users', username, 'cxml')):
+        for f in os.listdir(os.path.join(settings.BASE_DIR, 'data', 'users',
+                                         username, 'cxml')):
             f_split = os.path.splitext(f)
             if f_split[1] == '.xml':
-                profile = UserProfile(user=guest, module=f_split[0])
+                profile = UserProfile(user=user, module=f_split[0])
                 profile.save()
